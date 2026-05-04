7ebb9674058684221a3dcc70328604459af42f38
RomanChuykov <95050528+RomanChuykov@users.noreply.github.com>
2026-05-04 12:33:46 +0300

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://qacwmjsapfwqvyrvxapg.supabase.co/rest/v1/';
-const SUPABASE_KEY = 'sb_secret_8LlvLmLIS6W-Y-hHYPRrIw_8Nv10KOx';
+const SUPABASE_KEY = '123212321232123';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
